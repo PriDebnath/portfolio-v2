f91249ba82eab25299364a2f790047d73533983c
Pritam <debnathpritam0802@gmail.com>
2026-02-05 19:52:28 +0530

    better message popup

--- scripts/my.script.js
+++ scripts/my.script.js
@@ -2,7 +2,7 @@ let root = document.documentElement;
 let messagePopupAnimationDuration = getComputedStyle(root)
   .getPropertyValue('--message-popup-animation-stay');
 const delay = Number(messagePopupAnimationDuration) || 9000
-console.log(delay);
+// console.log(delay);
 
 let message = document.getElementById("message")
 if (message) {
