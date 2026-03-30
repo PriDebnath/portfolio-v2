54da9325137cb50a7feb9ee21ceea8ed1b14417b
Pritam <debnathpritam0802@gmail.com>
2026-03-30 14:15:55 +0530

    added custoizable cute avatar

---
+++ scripts/cute-avatar.script.js
@@ -0,0 +1,59 @@
+const buttonContainer = document.querySelector('[aria-label="head-decorators-button-container"]');
+const headDecoratorsImg = document.querySelector('[aria-label="head-decorator-img"]');
+
+const inactiveAllButtons = ()=>{
+const buttons = document.querySelectorAll('[aria-label="head-decorators-button-container"] button');
+buttons.forEach((pri)=>{
+    pri.classList.remove('active')
+})
+}
+
+const handleHeadDecoratorsBox = (event) => {
+  headDecoratorsImg.classList.add("decorator");
+
+  const btn = event.target;
+  const ariaLabel = btn.getAttribute("aria-label");
+
+  if (!ariaLabel || !/decorator-\d+/.test(ariaLabel)) return;
+
+  inactiveAllButtons()
+  btn.classList.add('active')
+  // remove previous animation classes
+  headDecoratorsImg.classList.remove(
+    "anim-bounce",
+    "anim-slide",
+    "anim-rotate",
+    "anim-hair"
+  );
+
+  // animate out
+  headDecoratorsImg.classList.add("animate-out");
+
+  setTimeout(() => {
+    // change image
+    headDecoratorsImg.src ="assets/images/cute-avatar/" + ariaLabel + ".png";
+
+    // choose animation type
+    if (ariaLabel.includes("heart") || ariaLabel.includes("flower")) {
+      headDecoratorsImg.classList.add("anim-bounce");
+    } else if (ariaLabel.includes("headphone")) {
+      headDecoratorsImg.classList.add("anim-slide");
+    } else if (ariaLabel.includes("cap")) {
+      headDecoratorsImg.classList.add("anim-rotate");
+    } else if (ariaLabel.includes("hair") || ariaLabel.includes("pony")) {
+      console.log("hair");
+      headDecoratorsImg.classList.add("anim-hair");
+    }
+
+    // animate in
+    headDecoratorsImg.classList.remove("animate-out");
+    headDecoratorsImg.classList.add("animate-in");
+  }, 200);
+};
+
+buttonContainer.addEventListener("click", handleHeadDecoratorsBox);
+
+const charector = document.querySelector('[aria-label="charector"]');
+
+/*
+ */
