ba8064a203d5aabcc0e950d5b78747073906881d
Pritam <debnathpritam0802@gmail.com>
2026-05-18 16:51:43 +0530

    #1 show intro regardless of hover #2 increse text size #3 make experience anr projects one after other

--- scripts/my.script.js
+++ scripts/my.script.js
@@ -4,12 +4,15 @@ let messagePopupAnimationDuration = getComputedStyle(root)
 const delay = Number(messagePopupAnimationDuration) || 9000
 // console.log(delay);
 
+let me = document.getElementById("me")
 let message = document.getElementById("message")
-if (message) {
-  setTimeout(() => {
-    message.style.animation = "none"
-  }, delay)
-}
+let messageCross = document.getElementById("message-cross")
+messageCross.addEventListener('click', () => {
+  message.classList.add('message-rotateX-90')
+},true)
+me.addEventListener('click', () => {
+  message.classList.remove('message-rotateX-90')
+},true)
 
 let getRandomNumber = (start, end) => {
   let randomNumber = Math.random() * (end - start) + start;
